7323e1a090419cbdb3d00394ff48577730d0e78e
Julius Carvajal <juliuscarvajal21@gmail.com>
2015-03-30 11:54:24 +1100

    Used recursion and setImmediate to avoid hitting the stack limit

--- dataGen.js
+++ dataGen.js
@@ -1,8 +1,11 @@
 /*jslint node:true*/
 'use strict';
 
+const MAX = 10000000;
+
 var moment = require('moment');
 var fs = require('fs');
+var fse = require('fs-extra');
 var helpers = require('./helpers');
 
 console.log("Begin Parsing >>");
@@ -11,31 +14,46 @@ var markTime = function(msg) {
     console.log(msg + ' : ' + moment.utc().format('YYYY-MM-DD[T]hh:mm:ss:ms'));
 };
 
-var bigdata = function* (max) {
-    markTime('start creating big array');
-    var i = 0;
-    while(i < max) {
-        var item = helpers.logData(helpers.helpers);
-        yield item;
-        i++;
-    }
-    
-    markTime('done creating big array');
-};
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-var write = (function() {
+var createFile = function() {
     var folder = 'data';
     var filename = 'balloon_' + moment.utc() + '.txt';
     var fullpath = folder + '/' + filename;
+    var file = fse.createOutputStream(fullpath); //fs.createWriteStream(fullpath);    
+    return file;
+}
 
-    markTime('start');
+var file = createFile();
+var write = function(i) {
+    if (i === undefined)
+        i = 0;
     
-    var file = fs.createWriteStream(fullpath);    
-    var data = bigdata(10000000);
+    if (i >= MAX)
+        return;
+    /*
+    if (getRandomInt(1, 10) % 4 === 0) {
+        file.end(); //the previous batch is done.
+        file = createFile(); //create a new file for the reader to process.
+    } 
+    */
     
-    for(var d of data) {
-        file.write(d + '\n');
+    var doWrite = function() {
+        var item = helpers.logData(helpers.helpers);
+        return file.write(item + '\n');
     }
     
-    markTime('end');
-})();
+    if (doWrite)    
+        process.stdout.write("Write: " + i.toLocaleString() + "\r");
+    else {
+        file.once('drain', doWrite);
+    }
+    
+    setImmediate(function() {
+        write(++i);
+    });
+};
+
+write();
--- logReader.js
+++ logReader.js
@@ -1,55 +1,75 @@
 /*jslint node:true*/
 'use strict';
-
+var log = require('single-line-log').stdout;
 var mongojs = require('mongojs');
 var db = mongojs('weatherBalloonDB', ['sourceData']);
 var watch = require('watch');
 var moment = require('moment');
 var fs = require('fs');
+var fse = require('fs-extra');
 var byline = require('byline');
 var helpers = require('./helpers').helpers;
 var folder = 'data';
 
 var commit = function(json) {
     db.sourceData.insert(json, function (err, docs) {
-        console.log("DB Insert Completed");
+        log("DB Insert Completed: " + docs.toString() + "\r");
     });
 };
 
 var data2json = function(data) {    
-    var json = {
-        timestamp: /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/.exec(data)[0],
-        location: /\d+,\d+/.exec(data)[0],
-        temperature: /\|(\d+)\|/.exec(data)[1],
-        observatory: /[A-Z]{2}/.exec(data)[0]
-    };
-    
-    commit(json);
+    setImmediate(function() {
+        var json = {
+            timestamp: /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/.exec(data)[0],
+            location: /\d+,\d+/.exec(data)[0],
+            temperature: /\|(\d+)\|/.exec(data)[1],
+            observatory: /[A-Z]{2}/.exec(data)[0]
+        };
+        commit(json);
+    });
 };
 
 var onChange = function(f, curr, prev) {
+    //console.log('onChange');
     if (/data\/balloon_\d+\.txt/.exec(f) == null)
         return;
         
     var file = byline(fs.createReadStream(f));
     file.on('data', data2json);
+    file.on('end', function() {
+        fs.unlink(f, function(err) {
+            //console.log(f + ' deleted.');
+        });
+    });
 };
 
-var onCreate = function(f, stat) {
-    // Handle new files
-};
+//var onCreate = ;
 
 var onRemove = function(f, stat) {
     // Handle removed files
+    //console.log('onRemove');
 }
+
+fse.ensureDir('./data', function(err) {
+    if (err)
+        console.log(err);
+});
 
 watch.createMonitor('./data', function(monitor) {
     console.log('watching directory');
     
-    monitor.on("created", onCreate);
-    monitor.on("changed", onChange);
+    monitor.on("created", function(f, stat) {
+        // Handle new files
+        //console.log('onCreate');
+        onChange(f);
+    });
+    
+    monitor.on("changed", function(f, curr, prev) {
+        //console.log('onChange');
+        onChange(f);
+    });
+
     monitor.on("removed", onRemove);
 });
 
 watch.unwatchTree('./data');
-
